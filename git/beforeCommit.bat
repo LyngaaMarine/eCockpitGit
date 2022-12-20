da01d1141e0aff8eaa4b22d5db5f3791f4bd55da
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2022-12-20 16:14:07 +0100

    Test_1

--- git/beforeCommit.bat
+++ git/beforeCommit.bat
@@ -1,2 +1,3 @@
 cd ..
 "C:\\Program Files (x86)\\WAGO Software\\e!COCKPIT\\e!COCKPIT.exe" --runscript='C:\O\LM\Shared - Docs\Application Shared Files\Git\eCockpit\exporter.py' --scriptargs='%__CD__%'
+python "C:\O\LM\Shared - Docs\Application Shared Files\Git\eCockpit\cleanup.py" "%__CD__%\"
